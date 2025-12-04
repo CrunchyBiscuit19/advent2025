834cde63e7a0d83ce61299b7d6eb85276dcff019
Sim Yue Yang <yueyangsim02@gamil.com>
2025-12-04 08:29:34 +0800

    day 3 part 2

--- q3/q3p2.cpp
+++ q3/q3p2.cpp
@@ -11,79 +11,38 @@
 #include <chrono>
 #include <cassert>
 
+#define NUM_BATTERIES 12
+
 int main() {
     // Timer start
     auto start = std::chrono::high_resolution_clock::now();
     
     // Input handler
-    std::ifstream file("test1");
+    std::ifstream file("test0");
     std::string line;
     if (!file.is_open()) return 0;
 
     // Answer
     long out = 0;
-    std::string constructedInt = "            "; // 12 spaces for 12 integers
     while (std::getline(file, line)) {
-        long lineMax = 0;
+        line.erase(remove_if(line.begin(), line.end(), isspace), line.end()); // Strip all whitespaces from the lines
+        long constructed = 0; 
+        int currIndex = 0;
+        int digitsRemaining = NUM_BATTERIES;
+        int windowSize = line.size() + 1 - digitsRemaining - currIndex; 
 
-        // Store per-bank indices of each individual digit
-        std::unordered_map<int, std::priority_queue<int, std::vector<int>, std::greater<int>>> bankIndices;
-        for (int i = 0; i < line.size(); i++) { 
-            int enteredBattery = line[i] - '0';
-            bankIndices.try_emplace(enteredBattery);
-            bankIndices.at(enteredBattery).push(i);
-        }
-        auto bankIndicesCopy = bankIndices;        
-        
-        // Test at each starting point
-        for (int i = 0; i <= line.size() - 12; i++) {
-            int constructedDigits = 0;
-            bankIndicesCopy = bankIndices;
-            // Initialize for each starting point
-            constructedInt[0] = line[i];
-            bankIndices.at(line[i] - '0').pop();
-            bankIndicesCopy.at(line[i] - '0').pop();
-            constructedDigits++;
-            int windowStart = i + 1;
-            int digitsRemaining = 10;
-            int windowSize = line.size() - digitsRemaining - windowStart;
+        for (int i = NUM_BATTERIES - 1; i >= 0; i--) {
+            auto digitIt = std::max_element(line.begin() + currIndex, line.begin() + currIndex + windowSize);
+            constructed += (*digitIt - '0') * pow(10, i);
 
-            // Pop from, adjust, and creating sliding window until windowSize == 1
-            while (windowSize > 1 && constructedDigits != 12) {
-                // Construct slidingWindow
-                std::priority_queue<int> slidingWindow;
-                for (int j = 0; j < windowSize; j++) slidingWindow.push(line[windowStart + j] - 48); // 'n' -> n
-                 // Place max in slidingWindow into string
-                constructedInt[12 - digitsRemaining - 1] = static_cast<char>(slidingWindow.top() + 48); // n -> 'n'
-                constructedDigits++;
-                // Adjust window settings for next iteration
-                windowStart = bankIndicesCopy.at(slidingWindow.top()).top() + 1;
-                
-                digitsRemaining--;
-                windowSize = line.size() - digitsRemaining - windowStart;
-                // Pop the leftmost index of the biggest integer within the sliding window
-                while (!slidingWindow.empty() && !bankIndicesCopy.at(slidingWindow.top()).empty() && bankIndicesCopy.at(slidingWindow.top()).top() < windowStart) {
-                    bankIndicesCopy.at(slidingWindow.top()).pop();
-                }
-            }
+            currIndex = std::distance(std::begin(line), digitIt) + 1;
+            digitsRemaining--;
+            windowSize = line.size() + 1 - digitsRemaining - currIndex;
+            assert(currIndex + digitsRemaining + windowSize == line.size() + 1);
+        }   
 
-            // Add rest after only 1 substring is left available
-            if (constructedDigits < 12) {
-                for (int j = windowStart; j < line.size(); j++) {
-                    constructedInt[12 - digitsRemaining - 1] = line[j];
-                    digitsRemaining--;
-                }
-            }
-
-            // Update max of each line
-            //std::cout << "Constructed " << constructedInt << std::endl;
-            lineMax = std::max(lineMax, std::stol(constructedInt));
-            
-            constructedInt = "            ";
-       }
-
-       std::cout << "Line Max " << lineMax << std::endl;
-       out += lineMax;
+        std::cout << "Line Max " << constructed << std::endl;
+        out += constructed;
     }
     file.close();
     std::cout << "Out Max " << out << std::endl;
